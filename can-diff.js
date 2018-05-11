464d4c47728f25746741ef2699e1cff77c985fb6
Justin Meyer <justinbmeyer@gmail.com>
2018-05-11 13:39:43 -0500

    export diff methods and namespace

--- can-diff.js
+++ can-diff.js
@@ -1,3 +1,16 @@
-export default function() {
-  return `This is the can-diff plugin`;
-}
+var deep = require("./deep/deep"),
+    list = require("./list/list"),
+    map = require("./map/map"),
+    mergeDeep = require("./merge-deep/merge-deep"),
+    Patcher = require("./patcher/patcher"),
+    namespace = require("can-namespace");
+    
+var diff = {
+    deep: deep,
+    list: list,
+    map: map,
+    mergeDeep: mergeDeep,
+    Patcher: Patcher
+};
+
+module.exports = namespace.diff = diff;
